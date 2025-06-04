69f5df3c43af67cea609444d62933c73f15c855c
matthewasdada <119554553+matthewasdada@users.noreply.github.com>
2025-06-04 00:53:48 +0000

    Set up Enemy sub-class of Character

--- char_test.py
+++ char_test.py
@@ -1,6 +1,7 @@
-from character import Character
+from character import Enemy
 
-harry = Character("harry", " A dirty, smelly Wumpus")
+harry = Enemy("harry", " A dirty, smelly Wumpus")
 harry.describe()
 harry.set_conversation("Come closer. I cannot see you..")
 harry.talk()
+print(harry)
