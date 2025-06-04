ed6c98057ba0ebc6d00a68929650c34094be9a7a
matthewasdada <119554553+matthewasdada@users.noreply.github.com>
2025-06-04 01:21:39 +0000

    Improvement in enemy sub-class, created weakness for characters to use against the Wumpus

--- char_test.py
+++ char_test.py
@@ -4,4 +4,7 @@ harry = Enemy("harry", " A dirty, smelly Wumpus")
 harry.describe()
 harry.set_conversation("Come closer. I cannot see you..")
 harry.talk()
-print(harry)
+harry.set_weakness(" vegemite")
+
+fight_with = input("What do you want to fight with?")
+harry.fight(fight_with)
--- character.py
+++ character.py
@@ -32,3 +32,18 @@ class Enemy(Character):
     """Defines attributes and methods for the Enemy sub-class"""
     def __init__(self, char_name, char_description):
         super().__init__(char_name, char_description)
+        self.weakness = None
+
+    def set_weakness(self, item_weakness):
+        self.weakness = item_weakness
+
+    def get_weakness(self):
+        return self.weakness
+    
+    def fight(self, combat_item):
+        if combat_item == self.weakness:
+            print("You fend off " + self.name + " with the " + combat_item)
+            return True
+        else:
+            print(self.name + " swallows you whole! You died.")
+            return False
